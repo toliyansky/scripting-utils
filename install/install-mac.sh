90f4e90a9ebba87014c3346d25b76cfef4aa2227
Anatolii Kozlov <anatolii.kozlov@ringcentral.com>
2024-06-05 19:29:31 +0400

    Add example of Java shebang script after install

--- install/install-mac.sh
+++ install/install-mac.sh
@@ -17,3 +17,21 @@ curl -s https://raw.githubusercontent.com/AnatoliyKozlov/scripting-utils/master/
 echo "Compile sources." &&
 javac --source 22 --enable-preview $INSTALL_DIR/scripting/Utils.java &&
 echo "Installation completed."
+
+RESET='\033[0m'
+BLUE='\033[34m'
+CYAN='\033[36m'
+GREEN='\033[32m'
+YELLOW='\033[33m'
+MAGENTA='\033[35m'
+
+echo -e "\nExample of Java shebang script:"
+echo -e "---------------------------------------------------------------------"
+echo -e "${YELLOW}#!/usr/bin/java --source 22 --enable-preview --class-path $INSTALL_DIR${YELLOW}${RESET}\n"
+echo -e "${BLUE}import${RESET} ${CYAN}static scripting.Utils${RESET}.${MAGENTA}*${RESET};\n"
+echo -e "${BLUE}void main${RESET}() {"
+echo -e "    ${CYAN}var response${RESET} = ${CYAN}http${RESET}.${GREEN}get${RESET}(\"${MAGENTA}https://httpbin.org/get${RESET}\");"
+echo -e "    ${CYAN}log${RESET}.${GREEN}info${RESET}(response.${GREEN}body${RESET}());"
+echo -e "}"
+echo -e "---------------------------------------------------------------------"
+echo -e "Don't forget apply chmod +x for your shebang\n"
